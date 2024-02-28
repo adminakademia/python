154afb9a634a144d13c4ac372dfb8511bd89bbef
adminakademia <104914726+adminakademia@users.noreply.github.com>
2024-02-28 12:58:21 +0100

    .

--- nornir/napalm_cli.py
+++ nornir/napalm_cli.py
@@ -1,10 +1,13 @@
 from nornir import InitNornir
 from nornir_napalm.plugins.tasks import napalm_cli
 from nornir_utils.plugins.functions import print_result
-
+from nornir_inspect import nornir_inspect
+from pprint import pprint
 
 nr=InitNornir(config_file="config.yaml")
 
 wynik=nr.run(napalm_cli,commands=["show run","show ip int br"])
 
 print_result(wynik)
+nornir_inspect(wynik)
+pprint(wynik["S1"][0].result["show ip int br"])
